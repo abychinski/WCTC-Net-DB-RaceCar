f1ad7b26dcb8fd423ccb11bc81b6f4675a25e839
Andrew Bychinski <aebychinski@live.com>
2022-10-05 18:35:07 -0500

    1

---
+++ RaceTrack/Cars/Wrangler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    internal class Wrangler : RaceCar
+    {
+       
+            public Wrangler()
+            {
+                Name = "Jeep";
+                TopSpeed = 90;
+            }
+
+            public override void StartEngine()
+            {
+                Console.WriteLine($"The {Name} starts like a dream!");
+            }
+
+            public override void Brake()
+            {
+                Console.WriteLine($"The {Name} stops by climbng a rock!");
+                base.Brake();
+            }
+        
+    }
+}
---
+++ RaceTrack/Drivers/Andrew.cs
@@ -0,0 +1,23 @@
+﻿using RaceTrack.RaceTrack.Cars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class Andrew : Driver
+    {
+        public Andrew(RaceCar car) : base(car)
+        {
+            Name = "Andrew";
+            SkillLevel = 10;
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+    }
+}
